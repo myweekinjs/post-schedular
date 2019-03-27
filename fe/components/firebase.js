95023230a4d8299ff8e79e88793ccb90e8367e92
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-27 21:21:22 +1100

    Setting up firebase class

--- fe/components/firebase.js
+++ fe/components/firebase.js
@@ -1,16 +1,43 @@
 import getConfig from 'next/config'
 const { publicRuntimeConfig } = getConfig()
-import firebase from "firebase";
+import app from "firebase/app"
+import "firebase/auth"
+import "firebase/database"
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: `${publicRuntimeConfig.NEXT_FIREBASE_apiKey}`,
-    authDomain: `${publicRuntimeConfig.NEXT_FIREBASE_authDomain}`,
-    databaseURL: `${publicRuntimeConfig.NEXT_FIREBASE_databaseURL}`,
-    projectId: `${publicRuntimeConfig.NEXT_FIREBASE_projectId}`,
-    storageBucket: `${publicRuntimeConfig.NEXT_FIREBASE_storageBucket}`,
-    messagingSenderId: `${publicRuntimeConfig.NEXT_FIREBASE_messagingSenderId}`
-  })
+const config = {
+  apiKey: `${publicRuntimeConfig.NEXT_FIREBASE_apiKey}`,
+  authDomain: `${publicRuntimeConfig.NEXT_FIREBASE_authDomain}`,
+  databaseURL: `${publicRuntimeConfig.NEXT_FIREBASE_databaseURL}`,
+  projectId: `${publicRuntimeConfig.NEXT_FIREBASE_projectId}`,
+  storageBucket: `${publicRuntimeConfig.NEXT_FIREBASE_storageBucket}`,
+  messagingSenderId: `${publicRuntimeConfig.NEXT_FIREBASE_messagingSenderId}`
 }
 
-export default firebase
+class Firebase {
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(config)
+    }
+
+    this.auth = app.auth()
+    this.db = app.database()
+
+    this.googleProvider = new app.auth.GoogleAuthProvider()
+  }
+
+  doSignInWithGoogle = () => (
+    this.auth.signInWithPopup(this.googleProvider)
+  )
+
+  doAuthCheck = () => (
+    this.auth.currentUser
+  )
+
+  doSetSessionPersistence = () => (
+    this.auth.setPersistence(app.auth.Auth.Persistence.SESSION)
+  )
+
+  scheduleTweet = (uid) => this.db.ref(`${uid}/tweets`)
+}
+
+export default Firebase
