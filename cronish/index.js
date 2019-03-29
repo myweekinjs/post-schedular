53ae176d7ce262daa32939ee59d425f9d7ea4429
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-03-29 14:18:19 +1100

    Documentation and separating some statements into functions

--- cronish/index.js
+++ cronish/index.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const admin = require('firebase-admin')
 const Twit = require('twit')
 
-// const key = require('./post-schedular-firebase-adminsdk-geihr-3f3bc9be31.json')
-
+// Initialise the Firebase app
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_projectId,
@@ -13,28 +12,69 @@ admin.initializeApp({
   databaseURL: "https://post-schedular.firebaseio.com"
 })
 
+/**
+ * Check if date is in the past
+ * 
+ * @param {Date} date (Date to check if it is in the past)
+ * @return {boolean}
+ */
+const isPastPost = (date) => {
+  return date < Date.now()
+}
+
+/**
+ * Deletes a given post from the Firebase Database
+ * 
+ * @param {string} ref (Firebase database ref as a string)
+ * @return false
+ */
+const deletePostFromFirebase = (ref) => {
+  admin.database.ref(ref).remove()
+}
+
+/**
+ * Checks if the tweets object has tweets to filter through
+ * 
+ * @param {object|null} tweets (Object of tweets returned by Firebase)
+ */
+const hasTweetsData = (tweets) => {
+  return tweets !== null && Object.keys(tweets).length > 0
+}
+
+/**
+ * Main schedular method
+ * 
+ */
 const schedular = () => {
+  // Creates a new twitter object to perform Twitter methods
   const Twitter = new Twit({
     consumer_key: process.env.TWITTER_consumer_key,
     consumer_secret: process.env.TWITTER_consumer_secret,
     access_token: process.env.TWITTER_access_token,
     access_token_secret: process.env.TWITTER_access_token_secret
   })
+  // Gets the Firebase user id
   const uid = process.env.FIREBASE_uid
+  // Tweet reference
   const tweetRef = admin.database().ref(`${uid}/tweets`)
   
+  // Get all the users tweets
   tweetRef.once('value')
     .then(snap => {
       const tweets = snap.val()
 
-      if (tweets !== null && Object.keys(tweets).length > 0) {
+      if (hasTweetsData(tweets)) {
+        // Loop through each tweet
         Object.keys(tweets).forEach(key => {
           const tweet = tweets[key]
   
-          if (tweet.date < Date.now()) {
+          // If the post is in the past
+          if (isPastPost(tweet.date)) {
+            // Create a Twitter post with the tweet value
             Twitter.post('statuses/update', { status: tweet.tweet }, function(err, data, response) {
               if (typeof err === 'undefined' || err === null) {
-                admin.database().ref(`${uid}/tweets/${key}`).remove()
+                // delete the tweet from the database
+                deletePostFromFirebase(`${uid}/tweets/${key}`)
               }
             })
           }
@@ -46,6 +86,8 @@ const schedular = () => {
     })
 }
 
+// Run schedular on deploy
 schedular()
 
-setInterval(schedular, 3000000)
+// Run every 30 minutes
+setInterval(schedular, 1800000)
